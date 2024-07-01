66ef1c8e49367f0b462fa8c1794f45d0634b6298
ImVux21 <vuodoan@gmail.com>
2024-07-01 23:04:30 +0700

    Changes

--- src/test/java/com/vudd/reactiveprogrammingplayground/ReactiveProgrammingPlaygroundApplicationTests.java
+++ src/test/java/com/vudd/reactiveprogrammingplayground/ReactiveProgrammingPlaygroundApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReactiveProgrammingPlaygroundApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
