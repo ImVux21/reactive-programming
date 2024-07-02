bd9f1dbe322dbe6e56d430826d4f057218bcfe9d
ImVux21 <vuodoan@gmail.com>
2024-07-02 23:12:27 +0700

    Just method to create a Mono ~ Publisher

---
+++ src/main/java/com/vudd/reactiveprogrammingplayground/sec02/Lec02MonoJust.java
@@ -0,0 +1,22 @@
+package com.vudd.reactiveprogrammingplayground.sec02;
+
+import com.vudd.reactiveprogrammingplayground.sec01.subscriber.SubscriberImpl;
+import reactor.core.publisher.Mono;
+
+public class Lec02MonoJust {
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("ball");
+        SubscriberImpl subscriber = new SubscriberImpl();
+        mono.subscribe(subscriber);
+
+        subscriber.getSubscription().request(5);
+        subscriber.getSubscription().request(5);
+        subscriber.getSubscription().cancel();
+
+//        mono.subscribe(
+//            item -> System.out.println("Received: " + item),
+//            err -> System.out.println("Error: " + err),
+//            () -> System.out.println("Completed")
+//        );
+    }
+}
